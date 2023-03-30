b4d771d6234883a05ea34cf32def2d062b82691e
Suraj S. Deshkar <103653512+SurajDeshkar24@users.noreply.github.com>
2023-03-30 22:20:34 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -54,6 +54,18 @@ Return your list ordered alphabetically by email starting with A. */
 
 /*Method 1 */
 
+SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
+FROM customer
+JOIN invoice ON invoice.customer_id = customer.customer_id
+JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
+JOIN track ON track.track_id = invoiceline.track_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+ORDER BY email;
+
+
+/* Method 2 */
+
 SELECT DISTINCT email,first_name, last_name
 FROM customer
 JOIN invoice ON customer.customer_id = invoice.customer_id
@@ -63,18 +75,6 @@ WHERE track_id IN(
 	JOIN genre ON track.genre_id = genre.genre_id
 	WHERE genre.name LIKE 'Rock'
 )
-ORDER BY email;
-
-
-/* Method 2 */
-
-SELECT DISTINCT email AS Email,first_name AS FirstName, last_name AS LastName, genre.name AS Name
-FROM customer
-JOIN invoice ON invoice.customer_id = customer.customer_id
-JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
-JOIN track ON track.track_id = invoiceline.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
 ORDER BY email;
 
 
